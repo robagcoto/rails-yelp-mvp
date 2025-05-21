2e657de05474cdaa2cf71445fc8973794d60aeef
Roberto Garcia <rgarcia2893@gmail.com>
2025-05-21 16:03:08 +0200

    completed restaurant navigation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create!(name: "Baby Love Burger", address: "63 Rue Saint-Maur, 75011 Paris", category: "french")
+Restaurant.create!(name: "Chez BAMI", address: "75 Rue Saint-Maur, 75011 Paris", phone_number: "01 43 55 34 25", category: "chinese")
+Restaurant.create!(name: "El Comal", address: "55 Rue Saint-Maur, 75011 Paris", phone_number: "01 42 23 50 51", category: "belgian")
+Restaurant.create!(name: "Cot Cot Gardette", address: "22 Rue Rochebrune, 75011 Paris", phone_number: "0981894740", category: "french")
+Restaurant.create!(name: "Impact Berliner Kebap", address: "119 Rue du Chemin Vert, 75011 Paris", phone_number: "0652194928", category: "italian")
